8c344b94b87507769be913f1563b4aefe2ecefac
argieuwu <argiepardillo009@gmail.com>
2025-08-30 12:02:42 +0800

    testing functional

--- bigbros-admin-testing.py
+++ bigbros-admin-testing.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 import time
+from selenium.common.exceptions import TimeoutException
 
 options = Options()
 options.add_experimental_option('detach', True)
@@ -14,7 +15,7 @@ driver.get('https://admin--mobile-app.vercel.app/login')
 
 wait = WebDriverWait(driver, 10)
 
-# --------------Login input credentials-----------------------
+# --------------Login input credentials---------------------------------------------
 emailInput = driver.find_element(By.ID, 'email')
 emailInput.send_keys('fortest3960@gmail.com')
 passwordInput = driver.find_element(By.ID, 'password')
@@ -30,7 +31,8 @@ print(' ✔ Login success')
 # print('Navigating to Dashboard page')
 # time.sleep(3)
 
-# ----------------User Page-------------------------------------
+# ----------------User Page-----------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 user = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Users')))
 user.click()
 print(' ✔ Navigating to Users page')
@@ -39,31 +41,34 @@ assert user.is_displayed(), "❌ Users page did not load"
 
 adminTab = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="radix-_r_0_-trigger-admins"]')))
 adminTab.click()
-time.sleep(3)
-# -----------------------ADD NEW ADMIN-------------------------
+time.sleep(2)
+# -----------------------ADD NEW ADMIN------------------------------------------------------------
 addAdmin = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="radix-_r_0_-content-admins"]/div[1]/button')))
 addAdmin.click()
 
 adminEmail = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="admin-email"]')))
-adminEmail.send_keys('testing12345@gmail.com')
+adminEmail.send_keys('testing1a2@gmail.com')
 adminUsername = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="admin-username"]')))
 adminUsername.send_keys('testing12345')
 adminPassword = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="admin-password"]')))
 adminPassword.send_keys('admin123')
 addAdminButton = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="radix-_r_6_"]/form/div[5]/button[1]')))
 addAdminButton.click()
-print(' ✔ Create admin acct successfully')
 time.sleep(5)
+print(' ✔ Create admin acct successfully')
 
 
-# ------------------------Booking Page------------------------------
+
+# ------------------------Booking Page------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 bookings = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Bookings')))
 bookings.click()
 print(' ✔ Navigating to Booking page')
 time.sleep(3)
 assert bookings.is_displayed(), '❌ Booking page did not load'
 
-# -------------------------Filter Status Button--------------------------
+
+# -------------------------Filter Status Button--------------------------------------------------
 allStatusButton = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[2]/main/div/div/div[2]/div[1]/select')))
 allStatusButton.click()
 
@@ -71,51 +76,115 @@ select = Select(allStatusButton)
 
 select.select_by_value('all')
 print(" ☆ Selected: All Status")
-time.sleep(3)
+time.sleep(1)
 
 select.select_by_value("pending")
 print(" ☆ Selected: Pending")
-time.sleep(3)
+time.sleep(1)
 
 select.select_by_value("rescheduled")
 print(" ☆ Selected: Rescheduled")
-time.sleep(3)
+time.sleep(1)
 
 select.select_by_value("confirmed")
 print(" ☆ Selected: Confirmed")
-time.sleep(3)
+time.sleep(1)
 
 select.select_by_value("cancelled")
 print(" ☆ Selected: Cancelled")
-time.sleep(3)
+time.sleep(1)
 
 select.select_by_value("completed")
 print(" ☆ Selected: Completed")
-time.sleep(3)
+time.sleep(1)
 
 select.select_by_value('all')
 print(" ☆ Selected: All Status")
+time.sleep(1)
+
+
+# -------------------------Accepting Bookings---------------------------------------------
+# -----------------------------------------------------------------------------------------
+acceptBooking = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[2]/main/div/div/div[2]/div[2]/table/tbody/tr[1]/td[7]/div/button[1]')))
+acceptBooking.click()
+assert acceptBooking.is_displayed(), '❌ Accept button not click'
+
+
+responseMessage = driver.find_element(By.XPATH, '//textarea[@placeholder="Enter your response message..."]')
+responseMessage.send_keys('tessssssssssssssssssssssssssst')
+
+assert responseMessage.is_displayed(), '❌  Message not working'
 time.sleep(3)
 
+
+confirmBooking = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.bg-green-600')))
+confirmBooking.click()
+print("✔ Confirm Booking clicked")
+time.sleep(3)
+
+try:
+    resolveBookingConflict = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="radix-_r_f_"]/div[3]/button')))
+    resolveBookingConflict.click()
+    print("✔ Resolve Booking Conflict clicked")
+    time.sleep(2)
+except TimeoutException:
+    print("ℹ No conflict dialog appeared, continuing...")
+
+# -------------------------History Bookings---------------------------------------------
+wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, "div[data-state='open']")))
+
+historyButton = wait.until(EC.element_to_be_clickable(
+    (By.CSS_SELECTOR, "button:has(svg.lucide-history)")
+))
+historyButton.click()
+print("✔ History button clicked")
+time.sleep(4)
+
+
+
+dialog = wait.until(EC.visibility_of_element_located(
+    (By.CSS_SELECTOR, "div[data-state='open']"))
+)
+#
+#
+# closeButton = wait.until(EC.element_to_be_clickable((
+#     By.CSS_SELECTOR, "div[data-state='open'] button:has(svg.lucide-x)"
+# )))
+#
+# try:
+#     closeButton.click()
+#     print('✔ History dialog closed')
+# except:
+#     driver.execute_script(driver.execute_script("arguments[0].click();", closeButton))
+#     print('✔ History dialog closed')
+#
+# wait.until(EC.invisibility_of_element_located(
+#     (By.CSS_SELECTOR, "div[data-state='open']"))
+# )
+
 # ------------------------------Program Page--------------------------
+# -----------------------------------------------------------------------------------------
 programs = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Programs')))
 programs.click()
 print(' ✔ Navigating to Programs page')
 time.sleep(3)
 
 # ------------------------------Resources Page-------------------------
+# -----------------------------------------------------------------------------------------
 resources = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Resources')))
 resources.click()
 print(' ✔ Navigating to Resources page')
 time.sleep(3)
 
 # ------------------------------Chat Support Page-------------------------
-chatSupport = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Chat Support')))
+# -----------------------------------------------------------------------------------------
+chatSupport = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href='/chat']")))
 chatSupport.click()
 print(' ✔ Navigating to Chat Support page')
 time.sleep(3)
 
 # ------------------------------Settings Page--------------------------
+# -----------------------------------------------------------------------------------------
 settings = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Settings')))
 settings.click()
 print(' ✔ Navigating to Settings page')
